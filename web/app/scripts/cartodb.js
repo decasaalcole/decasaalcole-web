15cc6402bf70611bc511eeed91bb0681cafddfef
Jorge Sanz <xurxosanz@gmail.com>
2014-09-07 14:49:23 +0200

    load cartodb layer

--- web/app/scripts/cartodb.js
+++ web/app/scripts/cartodb.js
@@ -1,11 +1,33 @@
 define(
-	['mustache'], 
-	function (mustache) {
+	['mustache','leaflet','cartodbjs'], 
+	function (mustache,leaflet,cartodbjs) {
 		console.log("pasando por cartodb");
 
 		function cartojs(options){
 			this.url = options.url || 'http://decasaalcole.cartodb.com';
+		};
+
+		function getWhere(filter){
+			var conditions = [];
+			if (filter.regimen && filter.regimen !== undefined ){
+				var reg = filter.regimen === '0' ? 'false' : 'true';
+				conditions.push("regimen = " + reg);
+			}
+			if (filter.maxtime){
+				conditions.push("atime < " + filter.maxtime);
+			}
+			if (filter.tipo){
+				conditions.push("nived like '%"+filter.tipo+"%'");
+			}
+
+			if (conditions.length>0){
+				return "WHERE " + conditions.join(" AND ") 
+			} else {
+				return "";
+			}
 		}
+
+
 
 		cartojs.prototype = {
 			sql : 'with ftimes as (select cp_to cp, from_dist dist, from_time atime ' +
@@ -15,36 +37,44 @@ define(
 				  'select * from ttimes union select \'{{cp}}\' cp, 0 dist, 0 atime) ' +
 				  'select c.*, t.atime, t.dist adist from coles_cp c join totaltimes t ' +
 				  'on c.cp = t.cp {{&where}} order by t.atime, t.dist',
+			sqlViz : 'with ftimes as (select cp_to cp, from_dist dist, from_time atime from times where cp_from = \'{{cp}}\'), ttimes as (select cp_from cp, to_dist dist, to_time atime from times where cp_to = \'{{cp}}\'), totaltimes as (select * from ftimes union select * from ttimes union select \'{{cp}}\' cp, 0 dist, 0 atime) select c.cartodb_id, c.the_geom, c.the_geom_webmercator, c.regimen, c.despecific, c.localidad, ((t.atime)/60)::integer minutes, (t.dist/1000)::integer kms from coles_cp c join totaltimes t on c.cp = t.cp {{&where}}order by t.atime, t.dist',
 
 			getURL : function() {
 				return this.url; 
 			},
 
-			getAPIURL:function(filter){
-				var result,sqlstr;
+			getSQL:function(filter){
 				var view = {
 					cp: filter.cp,
-					where: function(){
-						var conditions = [];
-						if (filter.regimen && filter.regimen !== undefined ){
-							var reg = filter.regimen === '0' ? 'false' : 'true';
-							conditions.push("regimen = " + reg);
-						}
-						if (filter.maxtime){
-							conditions.push("atime < " + filter.maxtime);
-						}
-						if (filter.tipo){
-							conditions.push("nived like '%"+filter.tipo+"%'");
-						}
+					where: getWhere(filter)
+				};
+				return filter && filter.cp ? mustache.render(this.sql,view) : null;
+			},
 
-						return "WHERE " + conditions.join(" AND ");
-					}
-				}
-				if (filter && filter.cp){
-					sqlstr = mustache.render(this.sql,view);
-				}
+			getSQLViz:function(filter){
+				var view = {
+					cp: filter.cp,
+					where: getWhere(filter)
+				};
+				return filter && filter.cp ? mustache.render(this.sqlViz,view) : null;
+			},
+
+			getAPIURL:function(filter){				
 				//http://decasaalcole.cartodb.com/api/v2/sql?q=
-				return this.url + "/api/v2/sql?q=" + encodeURI(sqlstr);
+				return this.url + "/api/v2/sql?q=" + encodeURI(this.getSQL(filter));
+			},
+
+			getLeafletLayer:function(filter,map){
+				// create a layer with 1 sublayer
+				cartodb.createLayer(map, {
+				  user_name: 'decasaalcole',
+				  type: 'cartodb',
+				  sublayers: [{
+				    sql: this.getSQLViz(filter),
+				    cartocss: '#times{marker-fill-opacity: 0.8; marker-line-color: #FFF; marker-line-width: 0; marker-line-opacity: 1; marker-width: 10; marker-fill: #FFFFB2; marker-allow-overlap: true; } #times [ minutes <= 500] {marker-fill: #B10026; } #times [ minutes <= 180] {marker-fill: #E31A1C; } #times [ minutes <= 150] {marker-fill: #FC4E2A; } #times [ minutes <= 120] {marker-fill: #FD8D3C; } #times [ minutes <= 90] {marker-fill: #FEB24C; } #times [ minutes <= 60] {marker-fill: #FED976; } #times [ minutes <= 30] {marker-fill: #FFFFB2;}'
+				  }]
+				})
+				.addTo(map) // add the layer to our map which already contains 1 sublayer
 			}
 
 		};
--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -2,10 +2,18 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         requirejs: '../bower_components/requirejs/require',
-        mustache: '../bower_components/mustache/mustache',        
+        mustache: '../bower_components/mustache/mustache',   
+        leaflet: '../bower_components/leaflet/dist/leaflet',
+        cartodbjs: '../bower_components/cartodb.js/dist/cartodb.noleaflet',     
         cartodb: './cartodb',
     },
     shim: {
+        'leaflet': {
+            exports: 'L'
+        },
+        'cartodbjs': {
+            deps: ['leaflet', 'jquery']
+        },
         
     }
 });
