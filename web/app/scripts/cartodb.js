24d8c6fb8b02d687257cdd2ca79f3a22ea95adbd
Vicent <visancal@gmail.com>
2014-09-19 17:58:45 +0200

    Solved the problem when reload the layer, updateing the layer sql

--- web/app/scripts/cartodb.js
+++ web/app/scripts/cartodb.js
@@ -49,16 +49,21 @@ define(
 			},
 
 			getLeafletLayer:function(filter,map){
-				// create a layer with 1 sublayer
-				cartodb.createLayer(map, {
-				  user_name: 'decasaalcole',
-				  type: 'cartodb',
-				  sublayers: [{
-				    sql: this.getSQL(filter),
-				    cartocss: '#times{marker-fill-opacity: 0.8; marker-line-color: #FFF; marker-line-width: 0; marker-line-opacity: 1; marker-width: 10; marker-fill: #FFFFB2; marker-allow-overlap: true; } #times [ minutes <= 500] {marker-fill: #B10026; } #times [ minutes <= 180] {marker-fill: #E31A1C; } #times [ minutes <= 150] {marker-fill: #FC4E2A; } #times [ minutes <= 120] {marker-fill: #FD8D3C; } #times [ minutes <= 90] {marker-fill: #FEB24C; } #times [ minutes <= 60] {marker-fill: #FED976; } #times [ minutes <= 30] {marker-fill: #FFFFB2;}'
-				  }]
-				})
-				.addTo(map) // add the layer to our map which already contains 1 sublayer
+				if(map._layers[25] !== undefined){
+					var lyr = map._layers[25];
+					var sublyr = lyr.getSubLayer(0);
+					sublyr.setSQL(this.getSQL(filter));
+				}else{
+					// create a layer with 1 sublayer
+					cartodb.createLayer(map, {
+					  user_name: 'decasaalcole',
+					  type: 'cartodb',
+					  sublayers: [{
+					    sql: this.getSQL(filter),
+					    cartocss: '#times{marker-fill-opacity: 0.8; marker-line-color: #FFF; marker-line-width: 0; marker-line-opacity: 1; marker-width: 10; marker-fill: #FFFFB2; marker-allow-overlap: true; } #times [ minutes <= 500] {marker-fill: #B10026; } #times [ minutes <= 180] {marker-fill: #E31A1C; } #times [ minutes <= 150] {marker-fill: #FC4E2A; } #times [ minutes <= 120] {marker-fill: #FD8D3C; } #times [ minutes <= 90] {marker-fill: #FEB24C; } #times [ minutes <= 60] {marker-fill: #FED976; } #times [ minutes <= 30] {marker-fill: #FFFFB2;}'
+					  }]
+					}).addTo(map) // add the layer to our map which already contains 1 sublayer
+				}
 			}
 
 		};
